1333214c4f09f76b53b3f375d759f9f9d15b892c
Pratham Patel <ppratham812@gmail.com>
2024-09-17 16:38:14 +0530

    db connected and synced using entity framework core

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api.Models
 {
@@ -10,14 +7,13 @@ namespace api.Models
         public int Id { get; set; }
         public string Symbol { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
+        public string Industry { get; set; } = string.Empty;
 
         [Column(TypeName = "decimal(18,2)")]
         public decimal Purchase { get; set; }
 
         [Column(TypeName = "decimal(18,2)")]
         public decimal LastDiv { get; set; }
-
-        public string Industry { get; set; } = string.Empty;
 
         public long MarketCap { get; set; }
 
